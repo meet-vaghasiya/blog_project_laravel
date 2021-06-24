92fa237af9fc82427926c1b7467ccb37bd48c96d
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-06-24 23:26:05 +0530

    section 8 routing basics complete

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        Route::pattern('number', '[0-9]+');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,66 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return 'second route';
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return 'contact page';
+    // return view('welcome');
+})->name('contact.index');
+
+
+Route::get('/about-us', function () {
+    return 'about us page';
+    // return view('welcome');
+})->name('about_us.index');
+
+
+Route::get('/posts/1', function () {
+    return  'post 1';
+});
+
+
+Route::get('/posts/2', function () {
+    return  'post 2';
+});
+
+
+Route::get('/posts/{id}', function ($id) {
+    return  'post no:' . $id;
+});
+
+
+
+Route::get('/posts/{id}', function ($idd) {
+    return  'post no:' . $idd;
+});
+
+
+
+Route::get('/posts/{id}/{no}', function ($id, $no) {
+    return  'post no:' . $id . $no;
+});  //it's compalsary parameter, we can pass  any argument name in function of route
+
+
+//optoinal parament
+Route::get('/optional/{no?}', function ($id = 20) {
+    return  'post no:' . $id;
+});  //provide default argument,otherwise it will give error
+
+
+Route::get('/where/{id}', function ($id = 20) {
+    return  'whre condition:' . $id;
+})->where([
+    'id' => '[0-9]+'
+]);
+
+
+Route::get('/route-service-provider-constrained/{number}', function ($id = 20) {
+    return  'route-service-provider-constrained' . $id;
+});
+
+//if id is not number than give 404 error
