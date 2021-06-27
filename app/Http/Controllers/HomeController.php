cef1e1ee8a21cd436d349e0b2c64d41cf1604225
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-06-27 21:11:10 +0530

    one to one relation ship

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+
+
+        $author = Author::with('profile')->get();
+        dd($author);
+
         return view('home.index');
     }
     public function contact()
